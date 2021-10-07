1903189429b6e099e1ad6dc854f5d28a4bf98b7f
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-10-07 20:14:18 -0300

    Endpoint que cria turmas

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import app from "./app";
 import createStudent from "./endpoints/createStudent";
 import createTeacher from "./endpoints/createTeacher";
-import getAllStudents from "./endpoints/getAllStudents";
+import createClass from "./endpoints/createClass";
 
-app.get("/students", getAllStudents);
+// CRIAR ALUNO
 app.post("/students", createStudent);
+// CRIAR PROFESSOR
 app.post("/teachers", createTeacher);
+// CRIAR TURMA
+app.post("/classes", createClass);
