2604b2d662e8cc717a2e5600a73acf72eb27b64e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-10-07 19:37:23 -0300

    Endpoint que cria um aluno

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import app from "./app";
+import createStudent from "./endpoints/createStudent";
 import getAllStudents from "./endpoints/getAllStudents";
 
-app.get("/", getAllStudents);
+app.get("/students", getAllStudents);
+app.post("/students", createStudent);
