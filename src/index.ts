aad8db6a362cc3d6ce1baa07d2091144abde43b2
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-10-07 19:49:08 -0300

    Endpoint que cria professores

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import createStudent from "./endpoints/createStudent";
+import createTeacher from "./endpoints/createTeacher";
 import getAllStudents from "./endpoints/getAllStudents";
 
 app.get("/students", getAllStudents);
 app.post("/students", createStudent);
+app.post("/teachers", createTeacher);
