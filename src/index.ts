eca90dda128717737f94d2853e01afbc6fcc8ce5
Muri-lo <muriloss2019@hotmail.com>
2021-10-08 22:09:39 -0300

    O endpoint de buscar idade pelo id foi feita de acordo com a correção, mas não estou sabendo testar, portanto vou conferir amanhã

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import createTeacher from "./endpoints/createTeacher";
 import createClass from "./endpoints/createClass";
 import AddTeacher from "./endpoints/AddTeacher";
 import addStudents from "./endpoints/addStudents";
-
+import searchAgeByID from "./endpoints/searchAgeByID"
 // CRIAR ALUNO
 app.post("/students", createStudent);
 // CRIAR PROFESSOR
@@ -18,3 +18,5 @@ app.put("/teachers",AddTeacher)
 //ADICONAR ALUNO NA Turma
 app.put("/students",addStudents)
 
+//Mostra idade do Estudante por ID
+app.get("/student/:id", searchAgeByID)
